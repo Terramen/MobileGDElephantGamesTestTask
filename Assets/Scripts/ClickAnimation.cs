a2efe933ba9640f818790b59c1b833375b2ddc42
Terramen <mrdimon4eg@gmail.com>
2023-07-31 12:29:27 +0200

    Reimport some voice lines and add a voice playing on an animation click

--- Assets/Scripts/ClickAnimation.cs
+++ Assets/Scripts/ClickAnimation.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class ClickAnimation : MonoBehaviour
 {
     private Animator _animator;
+    private AudioSource _source;
     [SerializeField] private string _animationName;
+    [SerializeField] private AudioClip _soundOnClick;
 
     private void Start()
     {
+        _source = GetComponent<AudioSource>();
+        _source.clip = _soundOnClick;
         _animator = GetComponent<Animator>();
     }
 
@@ -21,6 +25,7 @@ public class ClickAnimation : MonoBehaviour
     private void PlayAnimationOnClick()
     {
         _animator.Play(_animationName);
+        _source.Play();
     }
 }
 
